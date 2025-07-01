655f7adb1712d342de44b4f85ecac3c91c75ffa3
samio11 <samiohasan6@gmail.com>
2025-07-01 16:20:54 +0600

    User Modification Done

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,13 +1,16 @@
 import { useGetAllUserQuery } from "@/redux/api/userApi";
 import React from "react";
 import UserCard from "./UserCard/UserCard";
+import AddUserModal from "./UserCard/AddUserModal";
 
 const User = () => {
   const { data } = useGetAllUserQuery(undefined);
   const userData = data?.totalData;
   return (
     <div className="space-y-3 mt-3">
-      <div></div>
+      <div>
+        <AddUserModal></AddUserModal>
+      </div>
       <div className="grid grid-cols-3 gap-3">
         {userData?.map((x) => (
           <UserCard key={x._id} userData={x}></UserCard>
---
+++ src/pages/UserCard/AddUserModal.tsx
@@ -0,0 +1,164 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useCreateUserMutation } from "@/redux/api/userApi";
+import { useForm, type FieldValues, type SubmitHandler } from "react-hook-form";
+import Swal from "sweetalert2";
+
+const AddUserModal = () => {
+  const form = useForm();
+  const [createUser] = useCreateUserMutation();
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const modefiedAge = Number(data.age);
+    const modefiedData = { ...data, age: modefiedAge };
+    try {
+      const result = await createUser(modefiedData);
+      if (result) {
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Task is created",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    } catch (err: any) {
+      Swal.fire({
+        position: "top-end",
+        icon: "error",
+        title: err.message,
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+  };
+  return (
+    <div>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="outline">Add User</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Add User</DialogTitle>
+            <DialogDescription className="sr-only">
+              Make changes to your profile here. Click save when you&apos;re
+              done.
+            </DialogDescription>
+          </DialogHeader>
+          {/* Main Content */}
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input {...field}></Input>
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input {...field}></Input>
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input {...field}></Input>
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="age"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Age</FormLabel>
+                    <FormControl>
+                      <Input {...field}></Input>
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="gender"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Gender</FormLabel>
+                    <FormControl>
+                      {/* Drop down */}
+                      <Select
+                        value={field.value}
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                      >
+                        <SelectTrigger className="w-[180px]">
+                          <SelectValue placeholder="Select Gender" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectItem value="male">Male</SelectItem>
+                          <SelectItem value="female">Female</SelectItem>
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button variant="outline">Cancel</Button>
+                </DialogClose>
+                <Button type="submit">Save changes</Button>
+              </DialogFooter>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default AddUserModal;
